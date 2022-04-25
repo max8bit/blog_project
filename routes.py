c9ac02578c591ac51281f6df74599dfe79963590
max8bit <max8bit@gmail.com>
2022-04-25 17:41:17 +0300

    Adding login form

--- routes.py
+++ routes.py
@@ -1,6 +1,7 @@
+from flask import render_template
 from app import app
-
+from forms import LoginForm
 
 @app.route('/')
 def main_page():
-    return '<h1>Hello!</h1>'
+    return render_template('index.html')
