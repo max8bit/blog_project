c069b0d3623a606db00fead7c079aa596bef93f7
max8bit <max8bit@gmail.com>
2022-04-25 18:28:53 +0300

    sdadc

--- routes.py
+++ routes.py
@@ -1,7 +1,16 @@
-from flask import render_template
+from flask import render_template, flash, redirect, url_for
 from app import app
 from forms import LoginForm
 
 @app.route('/')
 def main_page():
     return render_template('index.html')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash(f'Login request for user{form.username.data}')
+        return redirect(url_for('main_page'))
+    return render_template('login.html', title='Login page', form=form)
