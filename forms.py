486aaed1cd5095fca8bd9e0b44ce80ecd5584494
100Ballov <maxim.krasnov.2020@gmail.com>
2022-05-23 18:28:00 +0300

    gswfwexzg

--- forms.py
+++ forms.py
@@ -1,5 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import ValidationError, DataRequired, EqualTo
+from models import User
 
 
 class LoginForm(FlaskForm):  # пишу свою форму поверх базовой формы из фласка
@@ -7,3 +9,22 @@ class LoginForm(FlaskForm):  # пишу свою форму поверх баз�
     password = PasswordField('Пароль: ')
     remember_me = BooleanField('Запомнить меня')
     submit = SubmitField('Войти')
+
+
+class RegistrationForm(FlaskForm):
+    username = SubmitField('Имя пользователя', validators=[DataRequired()])
+    email = SubmitField('Email', validators=[DataRequired()])
+    password = SubmitField('Password', validators=[DataRequired()])
+    password_again = SubmitField('Password(подтвержденние): ',
+    validators=[DataRequired(), EqualTo('password')])
+    submit= SubmitField('Регистрация')
+
+    def check_username(self, username):
+        user = User.query.filter(username = username.data)
+        if user is not None:
+            raise ValidationError('Пользователь с таким ником уже зарегистрирован!')
+
+    def check_email(self, email):
+        user = User.query.filter(email = email.data)
+        if user is not None:
+            raise ValidationError('Пользователь с такой почтой уже зарегистрирован!')
--- routes.py
+++ routes.py
@@ -1,4 +1,9 @@
+from crypt import methods
+
+from alembic.autogenerate import render
 from flask import render_template, flash, redirect, url_for
+from sqlalchemy.sql.functions import current_user
+
 from app import app
 from forms import LoginForm
 
@@ -14,3 +19,9 @@ def login():
         flash(f'Login request for user{form.username.data}')
         return redirect(url_for('main_page'))
     return render_template('login.html', title='Login page', form=form)
+
+@app.route('/register', methods=['GET', 'POST'])
+def fregister():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    return render_template('registr.html', form=form)
