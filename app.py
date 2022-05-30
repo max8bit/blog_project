fc896955a5627b701e354198234bdb7d04a6ab43
max8bit <max8bit@gmail.com>
2022-05-30 17:20:47 +0300

    adding register template

--- app.py
+++ app.py
@@ -2,15 +2,19 @@ from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login import LoginManager
 
 
 app = Flask(__name__)
-app.config.from_object(Config)
-db =SQLAlchemy(app)
+app.config.from_object(Config)  # подключаю настройки сайта
+db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+login = LoginManager(app)  # подключаем функцию логирования для сайта
+
 
 from routes import *
 from models import *
+
 
 if __name__ == '__main__':
     app.run()
--- routes.py
+++ routes.py
@@ -1,11 +1,14 @@
-from crypt import methods
-
-from alembic.autogenerate import render
 from flask import render_template, flash, redirect, url_for
-from sqlalchemy.sql.functions import current_user
+from flask_login import current_user, login_user, logout_user, login_required
+from app import app, login
+from forms import LoginForm, RegistrationForm
+from models import User
 
-from app import app
-from forms import LoginForm
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))  # достает пользователя из базы данных и запоминает его
+
 
 @app.route('/')
 def main_page():
@@ -14,14 +17,34 @@ def main_page():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('main_page'))
     form = LoginForm()
-    if form.validate_on_submit():
-        flash(f'Login request for user{form.username.data}')
+    if form.validate_on_submit():  # если форма отправляется
+        user = User.query.filter_by(username=form.username.data)  # пытаюсь найти пользователя в БД по логину
+        if user is None or not user.check_password(form.password.data):
+            # если пользователь не найден в БД или пароль не совпал
+            return redirect(url_for('login'))  # вернуть пользователя на страницу входа
+        login_user(user, remember=form.remember_me.data)
         return redirect(url_for('main_page'))
     return render_template('login.html', title='Login page', form=form)
 
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('main_page'))
+
+
 @app.route('/register', methods=['GET', 'POST'])
-def fregister():
-    if current_user.is_authenticated:
-        return redirect(url_for('index'))
-    return render_template('registr.html', form=form)
+def register():
+    if current_user.is_authenticated:  # если пользователь вошел
+        return redirect(url_for('index'))  # перенаправим на главную
+    form = RegistrationForm()
+    return render_template('register.html', form=form)
+
+
+@app.route('/support')
+@login_required
+def support():
+    return render_template('support.html')
